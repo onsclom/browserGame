79c3c38cfe0adfea4c600c876f153aedb2c1661a
onsclom <onsclom@gmail.com>
2020-01-04 20:44:48 -0800

    PSQL heroku setup.

--- app.js
+++ app.js
@@ -3,6 +3,27 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+
+//CODE FOR PSQL
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
+//end
+
+
 app.use('/public',express.static(path.join(__dirname,'static')));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname,'static','index.html')));
